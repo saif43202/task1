d9831ade81640e991b4f87d01b75818e8bf5b78d
saif43202 <80760576+saif43202@users.noreply.github.com>
2021-11-18 10:14:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import Form from "./Form";
+import React from 'react';
+import Wow from "./Wow";
+
+export default function App() {
+  const[show,setShow]=React.useState(true)
+  return (
+      <Form />
+  );
+}
